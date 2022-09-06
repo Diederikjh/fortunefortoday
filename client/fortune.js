f4b11057094e778abf1aec2d0d4ce9f09bf67054
Diederik <diederikjhattingh@gmail.com>
2022-09-06 21:32:33 +0200

    Update fortune.js
    Move to darklang infra

--- client/fortune.js
+++ client/fortune.js
@@ -9,10 +9,10 @@ function getNewFortune() {
     });*/
 
     $.ajax({
-      url: "https://9bj8bks4p3.execute-api.us-west-2.amazonaws.com/prod/fortunefortoday-get-cookie"
+      url: "https://diederik-fortune-cookie.builtwithdark.com/cookie"
     })
       .done(function( data ) {
-           fortuneBody.innerHTML = data["body-json"].GetCookieResult;
+           fortuneBody.innerHTML = data.cookie;
       }).error( function(){
            console.log("Error retreiving fortune"); 
            fortuneBody.innerHTML = "";
